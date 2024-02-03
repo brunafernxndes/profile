eb9058c3dad80d886717218e7303f933d2e87da9
brunafernxndes <brunafernandess@outlook.com.br>
2024-02-03 16:11:20 -0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const box = document.querySelector(".carrossel");
+const imagens = document.querySelectorAll(".carrossel .fundo-certificado");
+
+let contador = 0;
+
+function slider() {
+    contador++;
+
+    if (contador > imagens.length - 1) {
+        contador = 0;
+    }
+
+    box.style.transform = `translateX(${-contador * 350}px)`;
+}
+
+setInterval( slider , 1000 );
