a81831980ac3fca402d7739b5ddb02ed61e3858a
brunafernxndes <brunafernandess@outlook.com.br>
2024-02-08 20:46:33 -0300

    Atualizações 20

---
+++ js/script-responsivo.js
@@ -0,0 +1,40 @@
+const slider = document.querySelectorAll('.slider');
+const btnPrev = document.getElementById('arrow-left');
+const btnNext = document.getElementById('arrow-right');
+
+let currentSlide = 0;
+
+if (window.matchMedia("(max-width: 991px)").matches) {
+
+function hideSlider() {
+  slider.forEach(item => item.classList.remove('on'))
+}
+
+function showSlider() {
+  slider[currentSlide].classList.add('on')
+}
+
+function nextSlider() {
+  hideSlider()
+  if(currentSlide === slider.length -1) {
+    currentSlide = 0
+  } else {
+    currentSlide++
+  }
+  showSlider()
+}
+
+function prevSlider() {
+  hideSlider()
+  if(currentSlide === 0) {
+    currentSlide = slider.length -1
+  } else {
+    currentSlide--
+  }
+  showSlider()
+}
+
+btnNext.addEventListener('click', nextSlider)
+btnPrev.addEventListener('click', prevSlider)
+
+}
--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
-const box = document.querySelector(".carrossel");
-const imagens = document.querySelectorAll(".carrossel .fundo-certificado");
+const box = document.querySelector(".carousel");
+const imagens = document.querySelectorAll(".carousel .fundo-certificado");
 
 let contador = 0;
 
-function slider() {
-    contador++;
-
-    if (contador > imagens.length - 1) {
-        contador = 0;
+if (window.matchMedia("(min-width: 992px)").matches) {
+    function slider() {
+        contador++;
+    
+        if (contador > imagens.length - 1) {
+            contador = 0;
+        }
+    
+        box.style.transform = `translateX(${-contador * 370}px)`;
     }
-
-    box.style.transform = `translateX(${-contador * 370}px)`;
+    
+    setInterval( slider , 2500 );
 }
-
-setInterval( slider , 2500 );
